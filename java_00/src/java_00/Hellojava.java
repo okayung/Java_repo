792a489b22f0850ed0cf0fb0bd07c63b1d3f9601
USER <USER@1.209.180.117>
2024-06-19 09:28:15 +0900

    <2024.06.19>  - 최초커밋/헬로자바

---
+++ java_00/src/java_00/Hellojava.java
@@ -0,0 +1,18 @@
+package java_00;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String test1 = "안뇽";
+		
+		System.out.print(test1);
+		
+		System.out.println("Hello Java!!");
+		
+		System.out.println("Hello Java");
+
+	}
+
+}
